080dfff20488253d037609d1f3dde0bc4dbe4e36
M4rk9696 <nimalan.m@protonmail.com>
2020-05-07 16:23:34 +0530

    Update URL

--- content.js
+++ content.js
@@ -4,43 +4,43 @@ module.exports = {
       title: "Home Blocks",
       active: true,
       desc: "Custom home page and custom quotes",
-      url: "https://github.com/M4rk9696/home-blocks",
+      url: "https://github.com/mark1626/home-blocks",
     },
     {
       title: "Spin To Win",
       active: true,
       desc: "React PWA Spinner",
-      url: "https://m4rk9696.github.io/spin-to-win/",
+      url: "https://mark1626.github.io/spin-to-win/",
     },
     {
       title: "Micro Blog",
       active: true,
       desc: "My microblog with <i>twtxt</i>",
-      url: "https://m4rk9696.github.io/micro-blog/twtxt.txt",
+      url: "https://mark1626.github.io/micro-blog/twtxt.txt",
     },
     {
       title: "Knowledge",
       active: true,
       desc: "A git repository of my knowledge",
-      url: "https://github.com/M4rk9696/knowledge",
+      url: "https://github.com/mark1626/knowledge",
     },
     {
       title: "Tiddier",
       active: true,
       desc: "Inject the magic CSS into your page",
-      url: "https://github.com/M4rk9696/Tiddier",
+      url: "https://github.com/mark1626/Tiddier",
     },
     {
       title: "DeBooks",
       active: true,
       desc: "A common bookmark aggregater",
-      url: "https://github.com/M4rk9696/de-books",
+      url: "https://github.com/mark1626/de-books",
     },
     {
       title: "Mish",
       active: true,
       desc: "A minimal theme for fish",
-      url: "https://github.com/M4rk9696/theme-mish",
+      url: "https://github.com/mark1626/theme-mish",
     }
   ]
 }
